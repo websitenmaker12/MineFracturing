5d86b201e8eb20c2b853278d20d2ce428e0007d7
websitenmaker12 <eidechse12@hotmail.com>
2014-05-15 18:08:42 +0200

    Remove failed sync

--- java/de/teamdna/mf/util/WorldUtil.java
+++ java/de/teamdna/mf/util/WorldUtil.java
@@ -4,14 +4,7 @@ import net.minecraft.world.World;
 import net.minecraft.world.chunk.Chunk;
 
 public class WorldUtil {
-<<<<<<< HEAD
 
-=======
-	
-	/**
-	 * Sets the given biome at the given coords.
-	 */
->>>>>>> c0323ea1c7ab45c5b27c1365f0c0a4aeafdf7b48
 	public static void setBiomeForCoords(World world, int x, int z, int biomeID) {
 		Chunk chunk = world.getChunkFromBlockCoords(x, z);
 		byte[] chunkArray = chunk.getBiomeArray();
