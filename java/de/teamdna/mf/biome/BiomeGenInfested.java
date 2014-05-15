9b7a05fdca5ffc3cf1ee94b32525660384545b75
wara286 <maximilian@transcdelighting.com>
2014-05-15 20:48:30 +0200

    Biome improvements

--- java/de/teamdna/mf/biome/BiomeGenInfested.java
+++ java/de/teamdna/mf/biome/BiomeGenInfested.java
@@ -1,24 +1,34 @@
 package de.teamdna.mf.biome;
 
+import net.minecraft.entity.monster.EntityCaveSpider;
+import net.minecraft.entity.monster.EntityZombie;
+import net.minecraft.world.biome.BiomeGenBase;
 import cpw.mods.fml.relauncher.Side;
 import cpw.mods.fml.relauncher.SideOnly;
-import net.minecraft.world.biome.BiomeGenBase;
 
 public class BiomeGenInfested extends BiomeGenBase{
 
 	public BiomeGenInfested(int par1) {
 		super(par1);
+		this.spawnableMonsterList.clear();
+		this.spawnableMonsterList.add(EntityZombie.class);
+		this.spawnableMonsterList.add(EntityCaveSpider.class);
+		this.spawnableCreatureList.clear();
+		
+		this.setTemperatureRainfall(12, 12);
+		this.flowers.clear();
+		this.getSkyColorByTemp(50);
 	}
 	
 	@SideOnly(Side.CLIENT)
     public int getBiomeGrassColor(int p_150558_1_, int p_150558_2_, int p_150558_3_) {
         double d0 = plantNoise.func_151601_a((double)p_150558_1_ * 0.0225D, (double)p_150558_3_ * 0.0225D);
-        return d0 < -0.1D ? 5011004 : 6975545;
+        return 0x3f3f3f;
     }
 
     @SideOnly(Side.CLIENT)
     public int getBiomeFoliageColor(int p_150571_1_, int p_150571_2_, int p_150571_3_) {
-        return 6975545;
+        return 0x3f3f3f;
     }
 
 }
