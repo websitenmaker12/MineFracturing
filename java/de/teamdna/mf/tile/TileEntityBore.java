73bba65e896260fbb78e654a7ce43aa310d90953
websitenmaker12 <eidechse12@hotmail.com>
2014-05-15 16:49:35 +0200

    Started with CoreRegistry

--- java/de/teamdna/mf/tile/TileEntityBore.java
+++ java/de/teamdna/mf/tile/TileEntityBore.java
@@ -5,25 +5,28 @@ import java.util.List;
 
 import net.minecraft.block.Block;
 import net.minecraft.tileentity.TileEntity;
+import net.minecraft.world.ChunkCoordIntPair;
 
 public class TileEntityBore extends TileEntity {
 
 	public final int maxBoreY = 8;
-	public final int chunkRadius = 4;
 	
-	public int state = -1; // Inactive: -1 ; Boring: 0 ; Injecting: 1
+	public int state = -1; // Inactive: -1 ; Boring: 0 ; Scanning Chunks: 1; Injecting: 2
 	public int boreY;
+	public int radius;
 	
 	public List<String> oreBlocks = new ArrayList<String>();
+	public List<ChunkCoordIntPair> chunkQueue = new ArrayList<ChunkCoordIntPair>();
+	public ChunkCoordIntPair currentScanningChunk;
 	
 	@Override
 	public void updateEntity() {
 		if(!this.worldObj.isRemote) {
-			
 			// Setups the bore
 			if(this.state == -1) {
 				this.state = 0;
 				this.boreY = this.yCoord;
+				this.changeChunkRadius(4);
 			}
 			
 			// Bores to a hole until it reaches maxBoreY
@@ -39,10 +42,26 @@ public class TileEntityBore extends TileEntity {
 				}
 			}
 			
-			// Starts infecting the world and earning resources
-			if(this.state == 1) {
+			// Scanning Chunks
+			if(this.state == 1 && (this.chunkQueue.size() > 0 || this.currentScanningChunk != null)) {
+				if(this.currentScanningChunk == null) this.currentScanningChunk = this.chunkQueue.get(0);
 			}
 			
+			// Starts infecting the world and earning resources
+			if(this.state == 2) {
+			}
+		}
+	}
+	
+	public void changeChunkRadius(int radius) {
+		this.radius = radius;
+		for(int x = -radius; x <= radius; x++) {
+			for(int z = -radius; z <= radius; z++) {
+				ChunkCoordIntPair coord = new ChunkCoordIntPair(this.xCoord >> 4 + x, this.zCoord >> 4 + z);
+				if(this.chunkQueue.contains(coord)) {
+					this.chunkQueue.add(coord);
+				}
+			}
 		}
 	}
 	
