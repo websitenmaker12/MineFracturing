a411074ecf4b99fd100f8fa63528a75d2380d7a8
wara286 <maximilian@transcdelighting.com>
2014-05-15 18:10:49 +0200

    Try to sync

--- java/de/teamdna/mf/tile/TileEntityBore.java
+++ java/de/teamdna/mf/tile/TileEntityBore.java
@@ -79,7 +79,7 @@ public class TileEntityBore extends TileEntity {
 			
 			// Starts infesting the world and earning resources
 			if(this.state == 2) {
-				WorldUtil.setBiomeForCoords(this.worldObj, this.xCoord >> 4, this.zCoord >> 4, 75);
+				//WorldUtil.setBiomeForCoords(this.worldObj, this.xCoord >> 4, this.zCoord >> 4, 75);
 			}
 		}
 	}
