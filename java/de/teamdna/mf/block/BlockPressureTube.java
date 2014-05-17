565acf08d8d51662823ca8e0b1fb4260afad47ef
wara286 <maximilian@transcdelighting.com>
2014-05-17 12:03:34 +0200

    Some work.

--- java/de/teamdna/mf/block/BlockPressureTube.java
+++ java/de/teamdna/mf/block/BlockPressureTube.java
@@ -4,6 +4,7 @@ import net.minecraft.block.Block;
 import net.minecraft.block.BlockContainer;
 import net.minecraft.block.material.Material;
 import net.minecraft.tileentity.TileEntity;
+import net.minecraft.world.IBlockAccess;
 import net.minecraft.world.World;
 import de.teamdna.mf.net.ClientProxy;
 import de.teamdna.mf.tile.TileEntityPressureTube;
@@ -44,4 +45,17 @@ public class BlockPressureTube extends BlockContainer {
 		return ClientProxy.coreRenderID;
 	}
 	
+	public void setBlockBoundsBasedOnState(IBlockAccess par1BlockAcces, int x, int y, int z)
+    {
+		TileEntityPressureTube tile = (TileEntityPressureTube) par1BlockAcces.getTileEntity(x, y, z);
+		
+		float minX = 0.25F;
+		float maxX = 0.75F;
+		float minY = 0.25F;
+		float maxY = 0.75F;
+		float minZ = 0.25F;
+		float maxZ = 0.75F;
+            
+		this.setBlockBounds(0.0F, 0.0F, 0.0F, 1.0F, 1.0F, 1.0F);
+    }
 }
