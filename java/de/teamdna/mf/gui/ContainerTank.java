f78454532e44083ef690f575286c5f79ac079dae
websitenmaker12 <eidechse12@hotmail.com>
2014-05-17 18:14:25 +0200

    Improving Guis part 1

--- java/de/teamdna/mf/gui/ContainerTank.java
+++ java/de/teamdna/mf/gui/ContainerTank.java
@@ -2,11 +2,28 @@ package de.teamdna.mf.gui;
 
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.inventory.Container;
+import net.minecraft.inventory.Slot;
 import net.minecraft.world.World;
+import de.teamdna.mf.gui.slot.FuelSlot;
+import de.teamdna.mf.tile.TileEntityTank;
 
 public class ContainerTank extends Container {
 
+	private TileEntityTank tile;
+	
 	public ContainerTank(EntityPlayer player, World world, int x, int y, int z) {
+		this.tile = (TileEntityTank)world.getTileEntity(x, y, z);
+		this.addSlotToContainer(new FuelSlot(this.tile, 0, 47, 52));
+		
+		for(int i = 0; i < 3; i++) {
+            for(int j = 0; j < 9; j++) {
+            	this.addSlotToContainer(new Slot(player.inventory, j + i * 9 + 9, 8 + j * 18, 84 + i * 18));
+            }
+        }
+
+        for(int i = 0; i < 9; i++) {
+            this.addSlotToContainer(new Slot(player.inventory, i, 8 + i * 18, 142));
+        }
 	}
 	
 	@Override
--- java/de/teamdna/mf/gui/GuiTank.java
+++ java/de/teamdna/mf/gui/GuiTank.java
@@ -1,25 +1,42 @@
 package de.teamdna.mf.gui;
 
-import org.lwjgl.opengl.GL11;
-
-import de.teamdna.mf.Reference;
 import net.minecraft.client.gui.inventory.GuiContainer;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.util.ResourceLocation;
+import net.minecraft.util.StatCollector;
 import net.minecraft.world.World;
+import net.minecraftforge.fluids.FluidRegistry;
+
+import org.lwjgl.opengl.GL11;
+
+import de.teamdna.mf.Reference;
+import de.teamdna.mf.tile.TileEntityTank;
 
 public class GuiTank extends GuiContainer {
 
 	private static final ResourceLocation guiBg = new ResourceLocation(Reference.modid, "textures/gui/guiTank.png");
 	
+	private TileEntityTank tile;
+	
 	public GuiTank(EntityPlayer player, World world, int x, int y, int z) {
 		super(new ContainerTank(player, world, x, y, z));
+		
+		this.tile = ((TileEntityTank)world.getTileEntity(x, y, z)).controllerTile;
 	}
 	@Override
 	protected void drawGuiContainerForegroundLayer(int par1, int par2) {
 	    int k = (this.width - this.xSize) / 2;
 	    int l = (this.height - this.ySize) / 2;
-		this.fontRendererObj.drawString("Chemicals Tank", 8, 5, 4210752);
+	    
+	    String s = StatCollector.translateToLocal("tile.tankController.name");
+		this.fontRendererObj.drawString(s, this.xSize / 2 - this.fontRendererObj.getStringWidth(s) / 2, 4, 4210752);
+		
+		// Draw fluid
+		System.out.println(FluidRegistry.getFluid(this.tile.tank.getFluid().fluidID).getFlowingIcon());
+		
+		// Overlay
+		this.mc.getTextureManager().bindTexture(guiBg);
+		this.drawTexturedModalRect(12, 13, 0, 166, 124, 65);
 	}
 	
 	@Override
