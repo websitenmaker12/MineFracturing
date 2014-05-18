a93a62fe0e559a1224e90b2e81b4112548486540
wara286 <maximilian@transcdelighting.com>
2014-05-18 20:11:51 +0200

    Textures are done! :D

--- java/de/teamdna/mf/block/BlockGenerator.java
+++ java/de/teamdna/mf/block/BlockGenerator.java
@@ -2,10 +2,16 @@ package de.teamdna.mf.block;
 
 import cpw.mods.fml.relauncher.Side;
 import cpw.mods.fml.relauncher.SideOnly;
+import de.teamdna.mf.Reference;
 import net.minecraft.block.Block;
 import net.minecraft.block.material.Material;
 import net.minecraft.client.renderer.texture.IIconRegister;
+import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.item.ItemStack;
+import net.minecraft.tileentity.TileEntityFurnace;
 import net.minecraft.util.IIcon;
+import net.minecraft.util.MathHelper;
+import net.minecraft.world.World;
 
 public class BlockGenerator extends Block{
 
@@ -25,8 +31,33 @@ public class BlockGenerator extends Block{
 
 	@SideOnly(Side.CLIENT)
 	public void registerBlockIcons(IIconRegister p_149651_1_) {
-		this.blockIcon = p_149651_1_.registerIcon("gen_side");
-		this.frontIcon = p_149651_1_.registerIcon("gen_front");
-		this.topIcon = p_149651_1_.registerIcon("gen");
+		this.blockIcon = p_149651_1_.registerIcon(Reference.modid + ":gen_side");
+		this.frontIcon = p_149651_1_.registerIcon(Reference.modid + ":gen_front");
+		this.topIcon = p_149651_1_.registerIcon(Reference.modid + ":gen");
 	}
+	
+	public void onBlockPlacedBy(World p_149689_1_, int p_149689_2_, int p_149689_3_, int p_149689_4_, EntityLivingBase p_149689_5_, ItemStack p_149689_6_)
+    {
+        int l = MathHelper.floor_double((double)(p_149689_5_.rotationYaw * 4.0F / 360.0F) + 0.5D) & 3;
+
+        if (l == 0)
+        {
+            p_149689_1_.setBlockMetadataWithNotify(p_149689_2_, p_149689_3_, p_149689_4_, 2, 2);
+        }
+
+        if (l == 1)
+        {
+            p_149689_1_.setBlockMetadataWithNotify(p_149689_2_, p_149689_3_, p_149689_4_, 5, 2);
+        }
+
+        if (l == 2)
+        {
+            p_149689_1_.setBlockMetadataWithNotify(p_149689_2_, p_149689_3_, p_149689_4_, 3, 2);
+        }
+
+        if (l == 3)
+        {
+            p_149689_1_.setBlockMetadataWithNotify(p_149689_2_, p_149689_3_, p_149689_4_, 4, 2);
+        }
+    }
 }
