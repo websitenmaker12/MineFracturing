bcfe5591e9f31e3f3bb13b1da422fcb6f2ff17de
wara286 <maximilian@transcdelighting.com>
2014-06-03 18:15:16 +0200

    Added textures

--- java/de/teamdna/mf/block/BlockGrindStone.java
+++ java/de/teamdna/mf/block/BlockGrindStone.java
@@ -1,8 +1,10 @@
 package de.teamdna.mf.block;
 
 import net.minecraft.block.material.Material;
+import net.minecraft.client.renderer.texture.IIconRegister;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.tileentity.TileEntity;
+import net.minecraft.util.IIcon;
 import net.minecraft.world.IBlockAccess;
 import net.minecraft.world.World;
 import cpw.mods.fml.relauncher.Side;
@@ -12,8 +14,10 @@ import de.teamdna.mf.tile.TileEntityGrindStone;
 
 public class BlockGrindStone extends BlockCore {
 
+	private IIcon topIcon;
 	public BlockGrindStone() {
 		super(Material.rock);
+		this.setBlockTextureName("mf:grinder");
 	}
 
 	@Override
@@ -27,25 +31,36 @@ public class BlockGrindStone extends BlockCore {
         return true;
     }
 	
-	@Override
-	public boolean renderAsNormalBlock() {
-		return false;
-	}
-	
-	@Override
-	public boolean isOpaqueCube() {
-		return false;
-	}
-	
-	@Override
-	public boolean shouldSideBeRendered(IBlockAccess par1BlockAccess, int par2, int par3, int par4, int par5) {
-		return true;
-	}
+//	@Override
+//	public boolean renderAsNormalBlock() {
+//		return false;
+//	}
+//	
+//	@Override
+//	public boolean isOpaqueCube() {
+//		return false;
+//	}
+//	
+//	@Override
+//	public boolean shouldSideBeRendered(IBlockAccess par1BlockAccess, int par2, int par3, int par4, int par5) {
+//		return true;
+//	}
+//	
+//	@SideOnly(Side.CLIENT)
+//	@Override
+//	public int getRenderType() {
+//		return ClientProxy.coreRenderID;
+//	}
 	
 	@SideOnly(Side.CLIENT)
-	@Override
-	public int getRenderType() {
-		return ClientProxy.coreRenderID;
+    public IIcon getIcon(int par1, int par2)
+    {
+        return par1 == 0 ? this.topIcon : (par1 == 1 ? this.topIcon : this.blockIcon);
+    }
+	
+	@SideOnly(Side.CLIENT)
+	public void registerBlockIcons(IIconRegister register) {
+		this.blockIcon = register.registerIcon(this.getTextureName());
+		this.topIcon = register.registerIcon(this.getTextureName() + "_top");
 	}
-
 }
