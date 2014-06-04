3f58d18c1ce36570023914c18b8f7cc445697e07
websitenmaker12 <eidechse12@hotmail.com>
2014-06-04 18:17:21 +0200

    Old, good standart

--- java/de/teamdna/mf/block/BlockGrindStone.java
+++ java/de/teamdna/mf/block/BlockGrindStone.java
@@ -1,23 +1,21 @@
 package de.teamdna.mf.block;
 
 import net.minecraft.block.material.Material;
-import net.minecraft.client.renderer.texture.IIconRegister;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.tileentity.TileEntity;
-import net.minecraft.util.IIcon;
 import net.minecraft.world.IBlockAccess;
 import net.minecraft.world.World;
 import cpw.mods.fml.relauncher.Side;
 import cpw.mods.fml.relauncher.SideOnly;
+import de.teamdna.mf.Reference;
 import de.teamdna.mf.net.ClientProxy;
 import de.teamdna.mf.tile.TileEntityGrindStone;
 
 public class BlockGrindStone extends BlockCore {
 
-	private IIcon topIcon;
 	public BlockGrindStone() {
 		super(Material.rock);
-		this.setBlockTextureName("mf:grinder");
+		this.setBlockTextureName(Reference.modid + ":grinder");
 	}
 
 	@Override
@@ -31,35 +29,25 @@ public class BlockGrindStone extends BlockCore {
         return true;
     }
 	
-//	@Override
-//	public boolean renderAsNormalBlock() {
-//		return false;
-//	}
-//	
-//	@Override
-//	public boolean isOpaqueCube() {
-//		return false;
-//	}
-//	
-//	@Override
-//	public boolean shouldSideBeRendered(IBlockAccess par1BlockAccess, int par2, int par3, int par4, int par5) {
-//		return true;
-//	}
-//	
-//	@SideOnly(Side.CLIENT)
-//	@Override
-//	public int getRenderType() {
-//		return ClientProxy.coreRenderID;
-//	}
-	
-	@SideOnly(Side.CLIENT)
-    public IIcon getIcon(int par1, int par2) {
-        return par1 == 0 ? this.topIcon : (par1 == 1 ? this.topIcon : this.blockIcon);
-    }
-	
-	@SideOnly(Side.CLIENT)
-	public void registerBlockIcons(IIconRegister register) {
-		this.blockIcon = register.registerIcon(this.getTextureName());
-		this.topIcon = register.registerIcon(this.getTextureName() + "_top");
+	@Override
+	public boolean renderAsNormalBlock() {
+		return false;
 	}
+	
+	@Override
+	public boolean isOpaqueCube() {
+		return false;
+	}
+	
+	@Override
+	public boolean shouldSideBeRendered(IBlockAccess par1BlockAccess, int par2, int par3, int par4, int par5) {
+		return true;
+	}
+	
+	@SideOnly(Side.CLIENT)
+	@Override
+	public int getRenderType() {
+		return ClientProxy.coreRenderID;
+	}
+	
 }
